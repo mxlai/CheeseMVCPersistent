da02acbc7f1d19c1e4a2a0bea4cb7b5a60f827d5
Max Lai <max.y.lai@gmail.com>
2018-03-19 20:47:57 -0400

    New CategoryController and AddCategoryViewModel

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Data;
+using CheeseMVC.ViewModels;
+using CheeseMVC.Models;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var cheeseCategories = context.Categories.ToList();
+            return View(cheeseCategories);
+        }
+
+        public IActionResult Add()
+        {
+            AddCategoryViewModel addCategoryViewModel = new AddCategoryViewModel();
+            return View(addCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel addCategoryViewModel)
+        {
+            CheeseCategory newCategory = new CheeseCategory
+            {
+                Name = addCategoryViewModel.Name
+            };
+
+            context.Categories.Add(newCategory);
+            context.SaveChanges();
+
+            return Redirect("/Category");
+        }
+    }
+}
